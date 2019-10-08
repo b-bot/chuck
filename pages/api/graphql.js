e27fb1403ca73631900dff3e807a08b5b904f613
b-b0t <byronpolley@live.com>
2019-10-08 23:54:43 +0200

    removed duplicate code

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,45 +1,13 @@
 import { ApolloServer } from 'apollo-server-micro';
-import gql from 'graphql-tag';
-
-const fetch = require('node-fetch');
-
-const baseURL = 'https://api.chucknorris.io/jokes/';
-
-// eslint-disable-next-line import/prefer-default-export
-export const typeDefs = gql`
-
-  type Category {
-    name: String
-  }
-
-  type Random {
-    id: String
-    value: String
-  }
-
-  type Query {
-    categories: [Category]
-    random(cat: String): Random
-  }
-`;
-
-// eslint-disable-next-line import/prefer-default-export
-export const resolvers = {
-  Query: {
-    categories: () => fetch(`${baseURL}categories`).then((res) => res.json())
-      .then((data) => data.map((name) => ({ name }))),
-    random: (parent, args) => {
-      const { cat } = args;
-      return fetch(`${baseURL}random?category=${cat}`).then((res) => res.json());
-    },
-  },
-};
+import { typeDefs } from '../../apollo/type-defs';
+import { resolvers } from '../../apollo/resolvers';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
   playground: true,
+  upload: false,
 });
 
 export const config = {
